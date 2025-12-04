31cd7f81e6f669c6226f846d5cdc5cb481f6c1bb
sebastien-paltrie-neo4j <sebastien.paltrie@neo4j.com>
2025-12-04 10:40:25 +0000

    CYPHER fix

--- api/dao/genres.py
+++ api/dao/genres.py
@@ -69,10 +69,12 @@ class GenreDAO:
                 WHERE m.imdbRating IS NOT NULL AND m.poster IS NOT NULL AND g.name <> '(no genres listed)'
                 WITH g, m
                 ORDER BY m.imdbRating DESC
+
                 WITH g, head(collect(m)) AS movie
+
                 RETURN g {
                     .name,
-                    movies: size((g)<-[:IN_GENRE]-()),
+                    movies: count { (g)<-[:IN_GENRE]-() },
                     poster: movie.poster
                 } AS genre
             """, name=name).single()
